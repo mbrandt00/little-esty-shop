f153f95de15fdee8da6ad60555e5cbab6ded70b9
Michael Brandt <msbrandt00@gmail.com>
2022-03-03 17:08:46 -0700

    Nager API initialization and views

---
+++ app/services/nager_api.rb
@@ -0,0 +1,14 @@
+
+
+class NagerApi
+
+    def conn 
+        Faraday.new(url: "https://date.nager.at")
+    end
+
+  def next_three_holidays
+    resp = conn.get("Api/v2/NextPublicHolidays/US")
+    json = JSON.parse(resp.body, symbolize_names: true)
+    json[0..2]
+  end
+end
