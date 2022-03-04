2bbd01eb7cabaf6d6bde8c2282d4f9a6cabf5fe4
Michael Brandt <msbrandt00@gmail.com>
2022-03-04 10:31:31 -0700

    Form testing

--- spec/features/merchants/bulk_discounts/new_spec.rb
+++ spec/features/merchants/bulk_discounts/new_spec.rb
@@ -1,13 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'New Bulk Discount' do 
-    it 'will do something' do 
+    it 'will create a new bulk discount' do 
         merchant = create(:merchant)
         visit(new_merchant_bulk_discount_path(merchant))
-        fill_in 'name', with: 'Christmas'
-        fill_in("discount", with: 10)
-        fill_in("quantity", with: 5)
-        click_button('Save')
+        
+        fill_in('bulk_discount[name]', with: 'Christmas')
+        fill_in("bulk_discount[discount]", with: 10)
+        fill_in("bulk_discount[quantity]", with: 5)
+        click_button('Create Bulk discount')
         expect(current_path).to eq(merchant_bulk_discounts_path(merchant))
+        expect(page).to have_content('Christmas')
+        expect(page).to have_content(10)
+        expect(page).to have_content(5)
     end
 end
--- spec/features/merchants/bulk_discounts/show_spec.rb
+++ spec/features/merchants/bulk_discounts/show_spec.rb
@@ -2,12 +2,19 @@ require 'rails_helper'
 
 RSpec.describe "Bulk Discount Show Page" do 
     before(:each) do
-        @discount = create(:bulk_discount)
+        @merchant = create(:merchant)
+        @discount = create(:bulk_discount, merchant: @merchant)
     end
     it 'will show the threshold quantity and percentage discount' do 
-        visit(merchant_bulk_discount_url(@discount.merchant, @discount))
+        visit(merchant_bulk_discount_url(@merchant, @discount))
         expect(page).to have_content(@discount.name)
         expect(page).to have_content(@discount.quantity)
         expect(page).to have_content(@discount.discount)
+    end
+    it 'will have a link to edit the bulk discount' do 
+        visit(merchant_bulk_discount_url(@merchant, @discount))
+        expect(page).to have_link("Edit Bulk Discount")
+        click_link("Edit Bulk Discount")
+        expect(current_path).to eq(edit_merchant_bulk_discount_path(@merchant, @discount))
     end
 end
